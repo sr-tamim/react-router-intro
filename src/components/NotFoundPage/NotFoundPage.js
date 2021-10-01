05b990490b075bcbc83d3359a83c3a3f961931ef
TAMIM <saifur.tamim@gmail.com>
2021-10-01 12:20:10 +0600

    goToHome link added in 404 page

--- src/components/NotFoundPage/NotFoundPage.js
+++ src/components/NotFoundPage/NotFoundPage.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import "./NotFoundPage.css";
 
 const NotFoundPage = () => {
+    const history = useHistory();
     return (
         <div id='not-found-page'>
             <span> 404 - Not Found </span>
+            <br />
+            <span id="home-link" onClick={() => history.push('/')}>Go to Home</span>
         </div>
     );
 };
